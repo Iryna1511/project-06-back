fef2d6d58d75bf7ff30d260cda352cb2a3307bbd
Oleksii <shcherbaalex77@gmail.com>
2024-08-29 10:02:24 -0400

    added base structure

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import setupServer from "./server.js";
+import initMongoDB from "./db/initMongoConnection.js";
+import createDirIfNotExists from "./utils/createDirIfNotExists.js";
+import { TEMP_UPLOAD_DIR } from "./constants/index.js";
+
+await initMongoDB();
+await createDirIfNotExists(TEMP_UPLOAD_DIR);
+
+setupServer();
---
+++ src/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import pino from "pino-http";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import { env } from "./utils/env.js";
+import errorHandler from "./middlewares/errorHandler.js";
+import notFoundHandler from "./middlewares/notFoundHandler.js";
+
+import authRouter from "./routes/auth.js";
+
+const PORT = Number(env("PORT", 3000));
+
+export default function setupServer() {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+  app.use(cookieParser());
+
+  app.use(
+    pino({
+      transport: {
+        target: "pino-pretty",
+      },
+    })
+  );
+
+  app.use(authRouter);
+
+  app.use("*", notFoundHandler);
+
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
