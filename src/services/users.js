cd51ed4f649edd671e15c0b2213213e5d8ff04e2
IrynaTopolna <itopolna2808@gmail.com>
2024-08-31 16:32:49 +0300

    Adds password change logic

--- src/services/users.js
+++ src/services/users.js
@@ -1,4 +1,6 @@
 import { UserCollection } from "../db/models/users.js";
+import createHttpError from "http-errors";
+import bcrypt from "bcrypt";
 
 export async function getUserById(userId) {
   const user = await UserCollection.findById(userId);
@@ -6,7 +8,44 @@ export async function getUserById(userId) {
 }
 
 export async function updateUser(userId, user) {
-  return UserCollection.findOneAndUpdate({ _id: userId }, user, {
+  const checkedUser = await UserCollection.findOne({ _id: userId });
+  if (checkedUser === null) throw createHttpError(404, "User not found");
+
+  const updatedUser = {
+    name: user.name,
+    email: user.email,
+    gender: user.gender,
+    waterRate: user.waterRate,
+  };
+
+  if (user.password) {
+    if (user.newPassword === undefined)
+      throw createHttpError(400, "newPassword is required");
+
+    const isEqualPassword = await bcrypt.compare(
+      user.password,
+      checkedUser.password
+    );
+
+    if (isEqualPassword === false) throw createHttpError(401, "Wrong password");
+
+    const encryptedPassword = await bcrypt.hash(user.newPassword, 10);
+
+    const updatedUserWithPass = {
+      password: encryptedPassword,
+      ...updatedUser,
+    };
+
+    return UserCollection.findOneAndUpdate(
+      { _id: userId },
+      updatedUserWithPass,
+      {
+        new: true,
+      }
+    );
+  }
+
+  return UserCollection.findOneAndUpdate({ _id: userId }, updatedUser, {
     new: true,
   });
 }
